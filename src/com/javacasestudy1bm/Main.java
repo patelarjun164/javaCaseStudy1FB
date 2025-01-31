9bb138117d6211da98ae8a553f9a080e472704fc
Arjun Patel <hackmech007@gmail.com>
2025-01-31 22:25:36 +0530

    main added

---
+++ src/com/javacasestudy1bm/Main.java
@@ -0,0 +1,12 @@
+package com.javacasestudy1bm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        Bank bm = new Bank("HDFC", "HDFC018FB");
+        bm.displayAllAccount();
+        System.out.println(bm.getBankName());
+//        sdasd
+    }
+}
